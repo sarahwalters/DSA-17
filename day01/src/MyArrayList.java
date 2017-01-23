6b33f8e60e7785755a74743562ac900e9c7e8bfa
Sarah Walters <swalters4925@gmail.com>
2017-01-23 14:04:23 -0500

    ADD: HW1 implementation

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -1,35 +1,60 @@
 public class MyArrayList {
     private Cow[] elems;
 	private int size;
+	private final int LOWERLIMIT = 10;
 
 	public MyArrayList() {
-		// TODO
+		elems = new Cow[LOWERLIMIT];
+		size = 0;
 	}
 
 	public MyArrayList(int capacity) {
-		// TODO
+		elems = new Cow[capacity];
+		size = 0;
 	}
 
 	public void add(Cow c) {
-		// TODO
+		elems[size] = c;
+		size++;
+		resize();
 	}
 
 	public int size() {
-		// TODO
-		return -1;
+		return size;
 	}
 
 	public Cow get(int index) {
-		// TODO
-		return null;
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
+		return elems[index];
 	}
 
 	public Cow remove(int index) {
-		// TODO
-		return null;
+		Cow removed = elems[index];
+		System.arraycopy(elems, index, elems, index - 1, size);
+		elems[size - 1] = null;
+		size--;
+		resize();
+		return removed;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+		if (index > size) {
+			throw new IndexOutOfBoundsException("Index out of bounds.");
+		}
+		System.arraycopy(elems, index, elems, index + 1, size);
+		elems[index] = c;
+		size++;
+		resize();
+	}
+
+	private void resize() {
+		if (size * 4 < elems.length || size == elems.length) {
+			int newSize = Math.max(size * 2, LOWERLIMIT);
+			Cow[] newElems = new Cow[newSize];
+			System.arraycopy(elems, 0, newElems, 0, size);
+			elems = newElems;
+		}
 	}
 }
